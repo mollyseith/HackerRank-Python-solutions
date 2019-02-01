7af6154993f1400d08806d9057790ba6a6ef9844
mollyseith <mollllyyyyy@yahoo.com>
2019-02-01 18:09:14 -0500

    Java solutions

--- SherlockAndTheValidString.py
+++ SherlockAndTheValidString.py
@@ -3,10 +3,18 @@
 #Constraints: 1 <= |s| <= 10^5, each character s[i] set of ascii[a-z]
 #Output format: print YES if string s is valid, otherwise, print NO
 
-import collections
-
-s = raw_input().strip()
-freq = collections.Counter(s)
-values = list(freq.values())
-values.sort()
-print('YES' if values.count(values[0]) == len(values) or (values.count(values[0]) == len(values) - 1 and values[-1] - values[-2] == 1) or (values.count(values[-1]) == len(values) - 1 and values[0] == 1) else 'NO')
+from collections import Counter
+ 
+s = raw_input()
+characters = Counter(s)
+character_counter = Counter(characters.values())
+ 
+if len(character_counter) == 1:
+    return True
+ 
+if len(character_counter) == 2:
+    for i in character_counter.values():
+        if i == 1:
+            return True
+ 
+return False
